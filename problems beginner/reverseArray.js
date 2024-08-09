1e35477a3a96ebadddfe893f53112b43a22300c5
[chethangowda001] <[chehangowda001@gmail.com]>
2024-08-09 23:13:15 +0530

    new problems addd

---
+++ problems beginner/reverseArray.js
@@ -0,0 +1,7 @@
+function reverse(arr){
+    return arr.reverse();
+}
+
+const arr = [1,2,3,4,5,6,7];
+
+console.log(reverse(arr));
---
+++ problems beginner/reverseString.js
@@ -0,0 +1,6 @@
+function reverseString(str){
+    return str.split("").reverse().join("");
+}
+
+let str = "vinisha"
+console.log(reverseString(str));

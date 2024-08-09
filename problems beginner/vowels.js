8fe80fa7e8815ca5ee53f5f4d1ca2b296e1d6957
[chethangowda001] <[chehangowda001@gmail.com]>
2024-08-10 00:09:10 +0530

    new problems addd

---
+++ problems beginner/vowels.js
@@ -0,0 +1,7 @@
+function vowel(str){
+    if(str.includes(a,e,i,o,u)){
+        return count++;
+    }
+}
+let str = "Create a functions thatll return an integer of the number of vowels found in a string";
+console.log(vowel(str));

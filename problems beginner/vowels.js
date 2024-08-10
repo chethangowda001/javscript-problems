3365d836765bd0c6fde71dadfdcb528df169b7d2
[chethangowda001] <[chehangowda001@gmail.com]>
2024-08-10 11:48:18 +0530

    new problems addd

--- problems beginner/vowels.js
+++ problems beginner/vowels.js
@@ -1,7 +1,25 @@
+// function vowel(str){
+//     if(str.includes(a,e,i,o,u)){
+//         return count++;
+//     }
+// }
+// let str = "Create a functions thatll return an integer of the number of vowels found in a string";
+// console.log(vowel(str));
+
 function vowel(str){
-    if(str.includes(a,e,i,o,u)){
-        return count++;
+    const srt1 = str.toLowerCase().split("");
+    let count = 0;
+    let charCount=0;
+    for(let char of srt1){
+         if(char==="a" || char==="e" || char==="i" || char=== "o" || char === "u" ){
+            count++
+         }
+         if (char){
+            charCount++;
+         }
     }
+    return charCount + " " +count;
 }
+
 let str = "Create a functions thatll return an integer of the number of vowels found in a string";
 console.log(vowel(str));
